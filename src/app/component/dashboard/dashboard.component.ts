c6487d4f1ea2dca2551a04c8a1afa72cacd49d15
suryateja2519 <98818573+suryateja2519@users.noreply.github.com>
2022-06-14 10:50:23 +0530

    Update dashboard.component.ts

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -9,14 +9,15 @@ import { Employee } from 'src/app/model/employee';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
 export class DashboardComponent implements OnInit {
 
   empDetail !: FormGroup;
-  empObj : Employee = new Employee();
   empList : Employee[] = [];
   empId: any[]=[];
   exId: any[]=[];
   del: any[]=[];
+  empListId: any[]=[];
 
   constructor(private formBuilder : FormBuilder) { }
 
@@ -35,29 +36,31 @@ export class DashboardComponent implements OnInit {
   addEmployee(){
     var newId=this.empDetail.value.id;
     var employ=this.empDetail.value;
+    var condition=employ.id>=1 && employ.name.length>=1 && employ.age>=21 && employ.age<=99 && employ.gender!="";
     if (this.empId.includes(newId)) {
       alert("This Employee Code is already existed in Database!");
+      this.noChange();
     }
-    else if(employ.id>=1 && employ.name.length>=1 && employ.age>=21 && employ.age<=99 && employ.gender!=""){
+    else if(condition){
         this.empId.push(this.empDetail.value.id);
         this.empList.push(this.empDetail.value);
         this.empDetail.reset();
       }
       else if(employ.id>=1 && employ.name.length=="" && employ.age>=21 && employ.age<=99 && employ.gender!=""){
         alert("Employee Name is not entered!");
-        noChange();
+        this.noChange();
       }
       else if(employ.id>=1 && employ.name.length>=1 && employ.age>="" && employ.gender!=""){
         alert("Employee Age is not entered!");
-        noChange();
+        this.noChange();
       }
       else if(employ.id>=1 && employ.name.length>=1 && employ.age>=21 && employ.age<=99 && employ.gender==""){
         alert("Employee Gender is not selected!");
-        noChange();
+        this.noChange();
       }
       else{
         alert("Data Insuffienct!");
-        noChange();
+        this.noChange();
       }
     }
 
@@ -70,27 +73,45 @@ export class DashboardComponent implements OnInit {
     const i=this.empId.indexOf(this.empDetail.value.id);
     this.exId.push(this.empDetail.value.id);
     this.del.push(i);
-    this.empDetail.reset();
     this.empId.splice(this.del[0],1);
+    const j=this.empList.findIndex(x=>x.id===this.exId[0]);
+    this.empListId.push(j);
+    console.log(j);
+    console.log(this.empDetail.value);
+
   }
 
   updateEmployee() {
-
+    var employ=this.empDetail.value;
+    var condition=employ.id>=1 && employ.name.length>=1 && employ.age>=21 && employ.age<=99 && employ.gender!="";
     if (this.empId.includes(this.empDetail.value.id)){
       alert("This Employee Code is already existed in Database!") 
-    }else{
-      this.empList.splice(this.del[0],1,this.empDetail.value);
-      this.empId.splice(this.del[0],1,this.empDetail.value.id);
+      this.reset();
+    }else if(this.exId[0]==this.empDetail.value.id && condition){
+      this.empList.splice(this.empListId[0],1,this.empDetail.value);
+      this.empId.splice(this.del[0],0,this.empDetail.value.id);
+      this.reset();
+    }
+    else if(this.exId[0]!=this.empDetail.value.id && condition){  
       this.empId.push(this.exId[0]);
-      this.empId.push(this.del[0]);
-      this.del.pop();
-      this.exId.pop();
-      this.noChange();
+      this.empId.push(this.empDetail.value.id);
+      this.empList.splice(this.empListId[0],1,this.empDetail.value)
 
+      this.reset();
+    }
+    else{
+      alert("Data Insuiffienct!");
     }
 
 }
     
+reset(){
+  this.del.pop();
+  this.exId.pop();
+  this.empListId.pop();
+  this.noChange();
+}
+
   noChange(){
    this.empDetail.reset();
   }
